cygwingitgadget/cygwin

commit 827743ab7684a09bc077c91f6206e16ea85881b4

Cygwin: symlink_native: allow linking to `..`
When running

	CYGWIN=winsymlinks:nativestrict ln -s .. abc

the counter-intuitive result is _not_ a symbolic link to `..`, but
instead to `../../$(basename "$PWD")`.

The reason for this is that the search for the longest common prefix
assumes that the link target is not a strict prefix of the parent
directory of the link itself.

Let's fix that.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

winsup/cygwin/path.cc
@@ -1855,9 +1855,18 @@ symlink_native (const char *oldpath, path_conv &win32_newpath)
       while (towupper (*++c_old) == towupper (*++c_new))
 	;
       /* The last component could share a common prefix, so make sure we end
-         up on the first char after the last common backslash. */
-      while (c_old[-1] != L'\\')
-	--c_old, --c_new;
+         up on the first char after the last common backslash.
+
+	 However, if c_old is a strict prefix of c_new (at a component
+	 boundary), or vice versa, then do not try to find the last common
+	 backslash. */
+      if ((!*c_old || *c_old == L'\\') && (!*c_new || *c_new == L'\\'))
+	c_old += !!*c_old, c_new += !!*c_new;
+      else
+	{
+	  while (c_old[-1] != L'\\')
+	    --c_old, --c_new;
+	}
 
       /* 2. Check if prefix is long enough.  The prefix must at least points to
             a complete device:  \\?\X:\ or \\?\UNC\server\share\ are the minimum
@@ -1882,8 +1891,10 @@ symlink_native (const char *oldpath, path_conv &win32_newpath)
 	  final_oldpath = &final_oldpath_buf;
 	  final_oldpath->Buffer = tp.w_get ();
 	  PWCHAR e_old = final_oldpath->Buffer;
-	  while (num-- > 0)
-	    e_old = wcpcpy (e_old, L"..\\");
+	  while (num > 1 || (num == 1 && *c_old))
+	    e_old = wcpcpy (e_old, L"..\\"), num--;
+	  if (num > 0)
+	    e_old = wcpcpy (e_old, L"..");
 	  wcpcpy (e_old, c_old);
 	}
     }

commit 76953626276032c930c8d6a36f47a3539295b954

Cygwin: symlink_native: allow linking to `.` again
In 827743ab76 (Cygwin: symlink_native: allow linking to `..`,
2025-06-20), I fixed linking to `..` (which had inadvertently
targeted an incorrect location prior to that fix), but inadvertently
broke linking to `.` (which would now try to pass the empty string as
`lpTargetFileName` to `CreateSymbolicLinkW()`, failing with an
`ERROR_INVALID_REPARSE_DATA` which would be surfaced as "Permission
denied").

Let's fix this by special-casing an empty string as path as referring to
the current directory.

Note: It is unclear to me why the `winsymlinks:nativestrict` code path
even tries to simplify the symbolic link's target path (e.g. turn an
absolute path into a relative one). As long as it refers to a regular
Win32 file or directory, I would think that even something like
`././c` should have only the slashes converted, not the path
simplified (i.e. `.\.\c` instead of `c`). But that's a larger
discussion, and I would like to have the bug worked around swiftly.

Fixes: 827743ab76 (Cygwin: symlink_native: allow linking to `..`, 2025-06-20)
Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

winsup/cygwin/path.cc
@@ -1895,7 +1895,10 @@ symlink_native (const char *oldpath, path_conv &win32_newpath)
 	    e_old = wcpcpy (e_old, L"..\\"), num--;
 	  if (num > 0)
 	    e_old = wcpcpy (e_old, L"..");
-	  wcpcpy (e_old, c_old);
+	  if (e_old == final_oldpath->Buffer && c_old[0] == L'\0')
+	    wcpcpy (e_old, L".");
+	  else
+	    wcpcpy (e_old, c_old);
 	}
     }
   /* If the symlink target doesn't exist, don't create native symlink.
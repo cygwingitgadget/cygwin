picolibc/picolibc

commit bb4285206207343984d975f735d15701fd3ed6e1

Cygwin: pty: Implement new pseudo console support.
- In this implementation, pseudo console is created for each native
  console app. Advantages and disadvantages of this implementation
  over the previous implementation are as follows.

  Advantages:
  1) No performance degradation in pty output for cygwin process.
      https://cygwin.com/pipermail/cygwin/2020-February/243858.html
  2) Free from the problem caused by difference of behaviour of control
     sequences between real terminal and pseudo console.
      https://cygwin.com/pipermail/cygwin/2019-December/243281.html
      https://cygwin.com/pipermail/cygwin/2020-February/243855.html
  3) Free from the problem in cgdb and emacs gud.
      https://cygwin.com/pipermail/cygwin/2020-January/243601.html
      https://cygwin.com/pipermail/cygwin/2020-March/244146.html
  4) Redrawing screen on executing native console apps is not necessary.
  5) cygwin-console-helper is not necessary for the pseudo console
     support.
  6) The codes for pseudo console support are much simpler than that
     of the previous one.

  Disadvantages:
  1) The cygwin program which calls console API directly does not work.
  2) The apps which use console API cannot be debugged with gdb. This
     is because pseudo console is not activated since gdb uses
     CreateProcess() rather than exec(). Even with this limitation,
     attaching gdb to native apps, in which pseudo console is already
     activated, works.
  3) Typeahead key inputs are discarded while native console app is
     executed. Simirally, typeahead key inputs while cygwin app is
     executed are not inherited to native console app.
  4) Code page cannot be changed by chcp.com. Acctually, chcp works
     itself and changes code page of its own pseudo console.  However,
     since pseudo console is recreated for another process, it cannot
     inherit the code page.
  5) system_printf() does not work after stderr is closed. (Same with
     cygwin 3.0.7)

winsup/cygwin/dtable.cc
@@ -147,38 +147,6 @@ dtable::get_debugger_info ()
 void
 dtable::stdio_init ()
 {
-  for (int i = 0; i < 3; i ++)
-    {
-      const int chk_order[] = {1, 0, 2};
-      int fd = chk_order[i];
-      fhandler_base *fh = cygheap->fdtab[fd];
-      if (fh && fh->get_major () == DEV_PTYS_MAJOR)
-	{
-	  fhandler_pty_slave *ptys = (fhandler_pty_slave *) fh;
-	  if (ptys->get_pseudo_console ())
-	    {
-	      bool attached = !!fhandler_console::get_console_process_id
-		(ptys->get_helper_process_id (), true);
-	      if (attached)
-		break;
-	      else
-		{
-		  /* Not attached to pseudo console in fork() or spawn()
-		     by some reason. This happens if the executable is
-		     a windows GUI binary, such as mintty. */
-		  FreeConsole ();
-		  if (AttachConsole (ptys->get_helper_process_id ()))
-		    {
-		      ptys->fixup_after_attach (false, fd);
-		      break;
-		    }
-		}
-	    }
-	}
-      else if (fh && fh->get_major () == DEV_CONS_MAJOR)
-	break;
-    }
-
   if (myself->cygstarted || ISSTATE (myself, PID_CYGPARENT))
     {
       tty_min *t = cygwin_shared->tty.get_cttyp ();

winsup/cygwin/tty.cc
@@ -234,20 +234,14 @@ tty::init ()
   was_opened = false;
   master_pid = 0;
   is_console = false;
-  attach_pcon_in_fork = false;
-  h_pseudo_console = NULL;
   column = 0;
+  h_pseudo_console = NULL;
   switch_to_pcon_in = false;
-  switch_to_pcon_out = false;
-  screen_alternated = false;
   mask_switch_to_pcon_in = false;
   pcon_pid = 0;
   term_code_page = 0;
-  need_redraw_screen = true;
   pcon_last_time = 0;
-  pcon_in_empty = true;
-  req_transfer_input_to_pcon = false;
-  req_flush_pcon_input = false;
+  pcon_start = false;
 }
 
 HANDLE
@@ -291,16 +285,6 @@ tty_min::ttyname ()
   device d;
   d.parse (ntty);
   return d.name ();
-}
-
-void
-tty::set_switch_to_pcon_out (bool v)
-{
-  if (switch_to_pcon_out != v)
-    {
-      wait_pcon_fwd ();
-      switch_to_pcon_out = v;
-    }
 }
 
 void

winsup/cygwin/tty.h
@@ -94,21 +94,13 @@ private:
   HANDLE _to_master;
   HANDLE _to_master_cyg;
   HPCON h_pseudo_console;
-  HANDLE h_helper_process;
-  DWORD helper_process_id;
-  HANDLE h_helper_goodbye;
-  bool attach_pcon_in_fork;
+  bool pcon_start;
   bool switch_to_pcon_in;
-  bool switch_to_pcon_out;
-  bool screen_alternated;
   bool mask_switch_to_pcon_in;
   pid_t pcon_pid;
   UINT term_code_page;
-  bool need_redraw_screen;
   DWORD pcon_last_time;
-  bool pcon_in_empty;
-  bool req_transfer_input_to_pcon;
-  bool req_flush_pcon_input;
+  HANDLE h_pcon_write_pipe;
 
 public:
   HANDLE from_master () const { return _from_master; }
@@ -138,7 +130,6 @@ public:
   void set_master_ctl_closed () {master_pid = -1;}
   static void __stdcall create_master (int);
   static void __stdcall init_session ();
-  void set_switch_to_pcon_out (bool v);
   void wait_pcon_fwd (void);
   friend class fhandler_pty_common;
   friend class fhandler_pty_master;

winsup/cygwin/fhandler.h
@@ -326,16 +326,16 @@ class fhandler_base
   virtual size_t &raixput () { return ra.raixput; };
   virtual size_t &rabuflen () { return ra.rabuflen; };
 
-  virtual bool get_readahead_valid () { return raixget () < ralen (); }
+  bool get_readahead_valid () { return raixget () < ralen (); }
   int puts_readahead (const char *s, size_t len = (size_t) -1);
-  virtual int put_readahead (char value);
+  int put_readahead (char value);
 
   int get_readahead ();
   int peek_readahead (int queryput = 0);
 
   void set_readahead_valid (int val, int ch = -1);
 
-  virtual int get_readahead_into_buffer (char *buf, size_t buflen);
+  int get_readahead_into_buffer (char *buf, size_t buflen);
 
   bool has_acls () const { return pc.has_acls (); }
 
@@ -1905,7 +1905,7 @@ class fhandler_termios: public fhandler_base
   int ioctl (int, void *);
   tty_min *_tc;
   tty *get_ttyp () {return (tty *) tc ();}
-  virtual int eat_readahead (int n);
+  int eat_readahead (int n);
 
  public:
   tty_min*& tc () {return _tc;}
@@ -2184,7 +2184,6 @@ private:
   static bool need_invisible ();
   static void free_console ();
   static const char *get_nonascii_key (INPUT_RECORD& input_rec, char *);
-  static DWORD get_console_process_id (DWORD pid, bool match);
 
   fhandler_console (void *) {}
 
@@ -2264,19 +2263,7 @@ class fhandler_pty_common: public fhandler_termios
     return fh;
   }
 
-  bool attach_pcon_in_fork (void)
-  {
-    return get_ttyp ()->attach_pcon_in_fork;
-  }
-  DWORD get_helper_process_id (void)
-  {
-    return get_ttyp ()->helper_process_id;
-  }
-  HPCON get_pseudo_console (void)
-  {
-    return get_ttyp ()->h_pseudo_console;
-  }
-  bool to_be_read_from_pcon (void);
+  void resize_pseudo_console (struct winsize *);
 
  protected:
   BOOL process_opost_output (HANDLE h,
@@ -2287,23 +2274,15 @@ class fhandler_pty_slave: public fhandler_pty_common
 {
   HANDLE inuse;			// used to indicate that a tty is in use
   HANDLE output_handle_cyg, io_handle_cyg;
-  DWORD pid_restore;
-  int fd;
 
   /* Helper functions for fchmod and fchown. */
   bool fch_open_handles (bool chown);
   int fch_set_sd (security_descriptor &sd, bool chown);
   void fch_close_handles ();
 
-  bool try_reattach_pcon ();
-  void restore_reattach_pcon ();
-  inline void free_attached_console ();
-
  public:
   /* Constructor */
   fhandler_pty_slave (int);
-  /* Destructor */
-  ~fhandler_pty_slave ();
 
   void set_output_handle_cyg (HANDLE h) { output_handle_cyg = h; }
   HANDLE& get_output_handle_cyg () { return output_handle_cyg; }
@@ -2315,9 +2294,6 @@ class fhandler_pty_slave: public fhandler_pty_common
   ssize_t __stdcall write (const void *ptr, size_t len);
   void __reg3 read (void *ptr, size_t& len);
   int init (HANDLE, DWORD, mode_t);
-  int eat_readahead (int n);
-  int get_readahead_into_buffer (char *buf, size_t buflen);
-  bool get_readahead_valid (void);
 
   int tcsetattr (int a, const struct termios *t);
   int tcgetattr (struct termios *t);
@@ -2354,20 +2330,12 @@ class fhandler_pty_slave: public fhandler_pty_common
     copyto (fh);
     return fh;
   }
-  void set_switch_to_pcon (int fd);
+  bool setup_pseudoconsole (STARTUPINFOEXW *si, bool nopcon);
+  void close_pseudoconsole (void);
+  void set_switch_to_pcon (void);
   void reset_switch_to_pcon (void);
-  void push_to_pcon_screenbuffer (const char *ptr, size_t len, bool is_echo);
   void mask_switch_to_pcon_in (bool mask);
-  void fixup_after_attach (bool native_maybe, int fd);
-  bool is_line_input (void)
-  {
-    return get_ttyp ()->ti.c_lflag & ICANON;
-  }
   void setup_locale (void);
-  void set_freeconsole_on_close (bool val);
-  void trigger_redraw_screen (void);
-  void pull_pcon_input (void);
-  void update_pcon_input_state (bool need_lock);
 };
 
 #define __ptsname(buf, unit) __small_sprintf ((buf), "/dev/pty%d", (unit))
@@ -2392,7 +2360,6 @@ public:
   int process_slave_output (char *buf, size_t len, int pktmode_on);
   void doecho (const void *str, DWORD len);
   int accept_input ();
-  int put_readahead (char value);
   int open (int flags, mode_t mode = 0);
   void open_setup (int flags);
   ssize_t __stdcall write (const void *ptr, size_t len);
@@ -2431,9 +2398,7 @@ public:
     copyto (fh);
     return fh;
   }
-
-  bool setup_pseudoconsole (void);
-  void transfer_input_to_pcon (void);
+  bool to_be_read_from_pcon (void);
 };
 
 class fhandler_dev_null: public fhandler_base

winsup/cygwin/fhandler_console.cc
@@ -1206,18 +1206,6 @@ fhandler_console::close ()
   if (con_ra.rabuf)
     free (con_ra.rabuf);
 
-  /* If already attached to pseudo console, don't call free_console () */
-  cygheap_fdenum cfd (false);
-  while (cfd.next () >= 0)
-    if (cfd->get_major () == DEV_PTYM_MAJOR ||
-	cfd->get_major () == DEV_PTYS_MAJOR)
-      {
-	fhandler_pty_common *t =
-	  (fhandler_pty_common *) (fhandler_base *) cfd;
-	if (get_console_process_id (t->get_helper_process_id (), true))
-	  return 0;
-      }
-
   if (!have_execed)
     free_console ();
   return 0;
@@ -3609,37 +3597,6 @@ fhandler_console::need_invisible ()
 
   debug_printf ("invisible_console %d", invisible_console);
   return b;
-}
-
-DWORD
-fhandler_console::get_console_process_id (DWORD pid, bool match)
-{
-  DWORD tmp;
-  DWORD num, num_req;
-  num = 1;
-  num_req = GetConsoleProcessList (&tmp, num);
-  DWORD *list;
-  while (true)
-    {
-      list = (DWORD *)
-	HeapAlloc (GetProcessHeap (), 0, num_req * sizeof (DWORD));
-      num = num_req;
-      num_req = GetConsoleProcessList (list, num);
-      if (num_req > num)
-	HeapFree (GetProcessHeap (), 0, list);
-      else
-	break;
-    }
-  num = num_req;
-
-  tmp = 0;
-  for (DWORD i=0; i<num; i++)
-    if ((match && list[i] == pid) || (!match && list[i] != pid))
-      /* Last one is the oldest. */
-      /* https://github.com/microsoft/terminal/issues/95 */
-      tmp = list[i];
-  HeapFree (GetProcessHeap (), 0, list);
-  return tmp;
 }
 
 DWORD

winsup/cygwin/fork.cc
@@ -134,36 +134,6 @@ child_info::prefork (bool detached)
 int __stdcall
 frok::child (volatile char * volatile here)
 {
-  cygheap_fdenum cfd (false);
-  while (cfd.next () >= 0)
-    if (cfd->get_major () == DEV_PTYM_MAJOR)
-      {
-	fhandler_base *fh = cfd;
-	fhandler_pty_master *ptym = (fhandler_pty_master *) fh;
-	if (ptym->get_pseudo_console ())
-	  {
-	    debug_printf ("found a PTY master %d: helper_PID=%d",
-			  ptym->get_minor (), ptym->get_helper_process_id ());
-	    if (fhandler_console::get_console_process_id (
-				ptym->get_helper_process_id (), true))
-	      /* Already attached */
-	      break;
-	    else
-	      {
-		if (ptym->attach_pcon_in_fork ())
-		  {
-		    FreeConsole ();
-		    if (!AttachConsole (ptym->get_helper_process_id ()))
-		      /* Error */;
-		    else
-		      break;
-		  }
-	      }
-	  }
-      }
-  extern void clear_pcon_attached_to (void); /* fhandler_tty.cc */
-  clear_pcon_attached_to ();
-
   HANDLE& hParent = ch.parent;
 
   sync_with_parent ("after longjmp", true);

winsup/cygwin/select.cc
@@ -1215,8 +1215,7 @@ verify_tty_slave (select_record *me, fd_set *readfds, fd_set *writefds,
 	   fd_set *exceptfds)
 {
   fhandler_pty_slave *ptys = (fhandler_pty_slave *) me->fh;
-  if (me->read_selected && !ptys->to_be_read_from_pcon () &&
-      IsEventSignalled (ptys->input_available_event))
+  if (me->read_selected && IsEventSignalled (ptys->input_available_event))
     me->read_ready = true;
   return set_bits (me, readfds, writefds, exceptfds);
 }
@@ -1229,8 +1228,6 @@ peek_pty_slave (select_record *s, bool from_select)
   fhandler_pty_slave *ptys = (fhandler_pty_slave *) fh;
 
   ptys->reset_switch_to_pcon ();
-  if (ptys->to_be_read_from_pcon ())
-    ptys->update_pcon_input_state (true);
 
   if (s->read_selected)
     {

cygwingitgadget/cygwin

commit 30ecf5dca947937d5dc15b1f6331a15fcfab7093

Cygwin: pty: detect pcon-backed pty for non-Cygwin-spawned children
When a Cygwin process (e.g. `bash` under MinTTY) spawns a native
Win32 child (e.g. `git.exe`) with pseudo console support enabled,
the child gets a pseudo console that bridges the pty. If that native
child then spawns a Cygwin grandchild (e.g. `vim`, `less`), the
grandchild inherits the pseudo console's console handles. In
`init_std_file_from_handle()`, the grandchild's msys2-runtime sees
`GetConsoleScreenBufferInfo()` succeed on those handles and, with
no valid `ctty` set, falls back to `FH_CONSOLE` and gives the
process `cons0` instead of connecting to the pty.

This causes scrollback clobbering in MinTTY because alternate screen
sequences (`ESC[?1049h` / `ESC[?1049l`) are handled by
`fhandler_console`'s `save_restore()` against the pseudo
console's buffer, which has no correspondence to MinTTY's scrollback.

Fix this in the existing console branch of
`init_std_file_from_handle()`: when there is no valid `ctty` and
we are about to fall back to `FH_CONSOLE`, first scan the shared
tty table for an entry whose `pcon_activated` is set and whose
`nat_pipe_owner_pid` is in our console's process list (via
`GetConsoleProcessList`). If found, parse the device as that pty
slave instead of as a real console. The handle is closed in either
fallback path, matching the existing `FH_CONSOLE` behavior.
`myself->ctty` is left untouched; the regular
`fhandler_pty_slave::open_setup()` path will set it via
`myself->set_ctty()` when the pty slave is opened.

The structure of `find_pcon_pty()` matters and is easy to get
wrong in case a keen developer would like to refactor this code in
the future. This code runs on every Cygwin process startup whose
parent is non-Cygwin, so the common path (no pty with an active
pseudo console) must remain free of expensive operations. Two
pitfalls to avoid: filtering tty entries with `tty::exists()`
looks correct but creates and destroys a named pipe per entry
(128 entries on every call), and hoisting the
`GetConsoleProcessList()` call out of the loop pays the
cross-process cost even when no candidate exists. The current
shape, a cheap shared-memory boolean check first and a lazily
fetched process list only on the first candidate, keeps the
common case at a handful of pointer reads.

Reported downstream at https://github.com/git-for-windows/git/issues/5303
and bisected to a Git for Windows release that upgraded the bundled
msys2-runtime from 3.3.6 (no pseudo console code) to 3.4.6 (the new
pseudo console architecture).

Fixes: bb4285206207 ("Cygwin: pty: Implement new pseudo console support.")

winsup/cygwin/dtable.cc
@@ -327,7 +327,17 @@ dtable::init_std_file_from_handle (int fd, HANDLE handle)
 	dev.parse (myself->ctty);
       else
 	{
-	  dev.parse (FH_CONSOLE);
+	  /* Check whether the inherited console is actually a pseudo
+	     console bridging a pty.  This happens when our non-Cygwin
+	     parent was itself spawned by a Cygwin process from a pty
+	     (e.g. bash spawning git.exe which then spawns vim).  In
+	     that case, connect to the pty slave instead of treating
+	     the handle as a real console. */
+	  int pcon_minor = cygwin_shared->tty.find_pcon_pty ();
+	  if (pcon_minor >= 0)
+	    dev.parse (FHDEV (DEV_PTYS_MAJOR, pcon_minor));
+	  else
+	    dev.parse (FH_CONSOLE);
 	  CloseHandle (handle);
 	  handle = INVALID_HANDLE_VALUE;
 	}

winsup/cygwin/local_includes/tty.h
@@ -175,6 +175,10 @@ public:
   void wait_fwd ();
   bool pty_input_state_eq (xfer_dir x) { return pty_input_state == x; }
   bool nat_fg (pid_t pgid);
+  bool has_active_pcon () const
+    { return pcon_activated && switch_to_nat_pipe; }
+  bool has_pcon_and_owner (DWORD pid) const
+    { return pcon_activated && switch_to_nat_pipe && nat_pipe_owner_pid == pid; }
   friend class fhandler_pty_common;
   friend class fhandler_pty_master;
   friend class fhandler_pty_slave;
@@ -193,6 +197,7 @@ public:
   int connect (int);
   void init ();
   tty_min *get_cttyp ();
+  int find_pcon_pty ();
   int attach (int n);
   static void init_session ();
   friend class lock_ttys;

winsup/cygwin/tty.cc
@@ -123,6 +123,43 @@ tty_list::init ()
     }
 }
 
+/* Search for a pty whose pseudo console owns our console.
+   Return tty minor number or -1 if not found.
+   Called from init_std_file_from_handle() for processes started by
+   non-Cygwin parents to detect that inherited console handles are
+   from a pcon-backed pty.
+
+   The cheap precondition (any tty with pcon active in shared memory)
+   short-circuits the common case where no pty has a pseudo console
+   active, avoiding the GetConsoleProcessList() LPC call entirely. */
+int
+tty_list::find_pcon_pty ()
+{
+  DWORD pids[128];
+  DWORD count = 0;
+  bool got_pids = false;
+
+  for (int i = 0; i < NTTYS; i++)
+    {
+      if (!ttys[i].has_active_pcon ())
+	continue;
+
+      /* Fetch the console process list lazily, only on first candidate. */
+      if (!got_pids)
+	{
+	  count = GetConsoleProcessList (pids, 128);
+	  if (!count)
+	    return -1;
+	  got_pids = true;
+	}
+
+      for (DWORD j = 0; j < count; j++)
+	if (ttys[i].has_pcon_and_owner (pids[j]))
+	  return i;
+    }
+  return -1;
+}
+
 /* Search for a free tty and allocate it.
    Return tty number or -1 if error.
  */